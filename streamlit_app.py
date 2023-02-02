ca127afe1625df39cf7f69e73e8d1547fe164308
martapl7 <124290813+martapl7@users.noreply.github.com>
2023-02-02 18:37:41 +0100

    Update streamlit_app.py
    changed the icons to work

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text(🥣 'Omega 3 & blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 'Avocado Toast')
+streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
