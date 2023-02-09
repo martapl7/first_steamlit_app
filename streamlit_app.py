3e174a7de79528822605378f94f08110b0300cc6
martapl7 <124290813+martapl7@users.noreply.github.com>
2023-02-09 14:20:25 +0100

    Update streamlit_app.py
    removed json query

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #puts the data un a sequence
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
