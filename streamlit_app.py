862684666e4e9db5271c660c1b36fd176d01fea2
martapl7 <124290813+martapl7@users.noreply.github.com>
2023-02-09 20:04:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,31 +39,9 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
-
+      fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalize)
 except URLError as e:
       streamlit.error()
-
-
-streamlit.header("View Our Fruit List- Add Your Favorites!")
-def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur:
-           my_cur.execute("select * from fruit_load_list")
-           return my_cur.fetchall()
-
-if streamlit.button('Get Fruit List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
-
-if streamlit.button('Get Fruit List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
       
-def insert_row_snowflake(new_fruit):
-      with my_cnx.cursor() as my_cur:
-            my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-            return "Thanks for adding " + new_fruit
-
